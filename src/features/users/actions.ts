1faed07ade196b7b64020ce5de53ae5d49ddca9d
Shubham Mishra <shubhammishra10101998@gmail.com>
2025-09-07 16:27:48 +0530

    onboarding page added to the codebase

---
+++ src/features/users/actions.ts
@@ -0,0 +1,17 @@
+'use server';
+
+import { cacheTag } from 'next/dist/server/use-cache/cache-tag';
+import { getUserIdTag } from './dbCache';
+import { db } from '@/drizzle/db';
+import { eq } from 'drizzle-orm';
+import { UserTable } from '@/drizzle/schema';
+
+// GET USER SERVER ACTIONS
+export async function getUser(id: string) {
+  'use cache';
+  cacheTag(getUserIdTag(id));
+
+  return db.query.UserTable.findFirst({
+    where: eq(UserTable.id, id),
+  });
+}
